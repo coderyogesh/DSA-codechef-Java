c9732fe61e4ba8a6fe29f9e0e86c16383faf99e3
yogeshjangid <yogeshrj1433@gmail.com>
2022-04-30 01:21:43 +0530

    First Program

---
+++ src/com/yogesh/NumberMirror.java
@@ -0,0 +1,14 @@
+package com.yogesh;
+
+import java.util.Scanner;
+
+public class NumberMirror {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number");
+        int a = sc.nextInt();
+        System.out.println("The number is:" +a);
+    }
+}
